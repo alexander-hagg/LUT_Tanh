d216c5d8a740f7bd999b4e3404322ab3a4a5758d
Caleb <caleb@tupac.cs.binghamton.edu>
2014-06-25 15:40:50 -0400

    switched from using constant magic look up values to calculated values look up values

--- lut_tanh.m
+++ lut_tanh.m
@@ -2,20 +2,17 @@ global _lBounds = [0.390625, 0.453125, 0.515625, 0.578125, 0.640625, \
 				   0.703125, 0.781250, 0.859375, 0.937500, 1.046875, \
 				   1.171875, 1.328125, 1.531250, 1.859375, 2.906250];
 
-global _rVals	= [0.4049  , 0.4558  , 0.5038  , 0.5490  , 0.5911  , \
-				   0.6348  , 0.6791  , 0.7190  , 0.7609  , 0.8057  , \
-				   0.8493  , 0.8916  , 0.9329  , 0.9740  , 1.0000  ];
-
 #NOTE: Hardware range converter requires atleast 3 integer bits 
 global _iBits   = 3;
 global _fBits   = 13;
 
 function val = LUT_Tanh(x)
 	global _lBounds;
-	global _rVals;
-	
+	rVals = genValues(_lBounds);
+	rVals(end + 1) = 1;
+
 	fLows = toFixed(_lBounds);
-	fVals = toFixed(_rVals);
+	fVals = toFixed(rVals);
 
 	s = sign(x);
 	fMag = toFixed(abs(x));
@@ -42,4 +39,21 @@ function setPrecision(ib, fb)
 	global _fBits;
 	_iBits = ib;
 	_fBits = fb;
+endfunction
+
+function rVals = genValues(bounds)
+	#create pairs
+	pairs = [];
+	rVals = [];
+	temp = bounds(1);
+	for i = bounds(2:end)
+		pairs(end + 1).a = temp;
+		pairs(end).b = i;
+		temp = i;
+	endfor
+	
+	#get avg value
+	for i = pairs
+		rVals(end + 1) = 1/(i.b - i.a) * quad(@(x) tanh(x), i.a, i.b);
+	endfor
 endfunction
