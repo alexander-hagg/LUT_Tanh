f8f8a9c8f923f0f55a27f09aa11ea54061124a38
Caleb <caleb@tupac.cs.binghamton.edu>
2014-06-25 15:54:33 -0400

    calculate rvals up front

--- lut_tanh.m
+++ lut_tanh.m
@@ -2,17 +2,36 @@ global _lBounds = [0.390625, 0.453125, 0.515625, 0.578125, 0.640625, \
 				   0.703125, 0.781250, 0.859375, 0.937500, 1.046875, \
 				   1.171875, 1.328125, 1.531250, 1.859375, 2.906250];
 
+#used to calculate return values
+function rVals = genValues(bounds)
+	#create pairs
+	pairs = [];
+	rVals = [];
+	temp = bounds(1);
+	for i = bounds(2:end)
+		pairs(end + 1).a = temp;
+		pairs(end).b = i;
+		temp = i;
+	endfor
+	
+	#get avg value
+	for i = pairs
+		rVals(end + 1) = 1/(i.b - i.a) * quad(@(x) tanh(x), i.a, i.b);
+	endfor
+endfunction
+
+global _rVals = [genValues(_lBounds), 1]
+
 #NOTE: Hardware range converter requires atleast 3 integer bits 
 global _iBits   = 3;
 global _fBits   = 13;
 
 function val = LUT_Tanh(x)
 	global _lBounds;
-	rVals = genValues(_lBounds);
-	rVals(end + 1) = 1;
+	global _rVals;
 
 	fLows = toFixed(_lBounds);
-	fVals = toFixed(rVals);
+	fVals = toFixed(_rVals);
 
 	s = sign(x);
 	fMag = toFixed(abs(x));
@@ -41,19 +60,4 @@ function setPrecision(ib, fb)
 	_fBits = fb;
 endfunction
 
-function rVals = genValues(bounds)
-	#create pairs
-	pairs = [];
-	rVals = [];
-	temp = bounds(1);
-	for i = bounds(2:end)
-		pairs(end + 1).a = temp;
-		pairs(end).b = i;
-		temp = i;
-	endfor
-	
-	#get avg value
-	for i = pairs
-		rVals(end + 1) = 1/(i.b - i.a) * quad(@(x) tanh(x), i.a, i.b);
-	endfor
-endfunction
+
